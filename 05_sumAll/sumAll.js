aa8149934cdd77cf5721f419e4f8661b9d3bef05
Jacob Templeton <jwctempleton@gmail.com>
2022-03-01 20:25:41 -0800

    6/6 tests pass for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  let total = 0;
+  if (firstNum < 0 || secondNum < 0) {
+    return "ERROR";
+  }
+  if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+    return "ERROR";
+  }
+  if (firstNum > secondNum) {
+    [firstNum, secondNum] = [secondNum, firstNum];
+  }
 
+  for (let i = firstNum; i <= secondNum; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
