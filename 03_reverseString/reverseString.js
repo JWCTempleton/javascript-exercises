59520fec23f3b06a2168bf7449ade5f77b1a1f87
Jacob Templeton <jwctempleton@gmail.com>
2022-03-01 17:18:07 -0800

    4/4 tests pass for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splitStr = str.split("").reverse();
+  let newStr = "";
+  for (let i = 0; i < splitStr.length; i++) {
+    newStr += splitStr[i];
+  }
+  return newStr;
 };
 
 // Do not edit below this line
