4b51a8fe7757184372f262617474b2619b29d775
Jacob Templeton <jwctempleton@gmail.com>
2022-03-05 13:34:51 -0800

    Add solutions to 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let sum = 1;
+  arr.forEach((element) => {
+    sum *= element;
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let total = 0;
+  for (let i = 0; i <= b; i++) {
+    total += a * a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  if (num === 0 || num === 1) {
+    return total;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
