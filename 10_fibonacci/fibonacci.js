4b1c2efa3f50b53ed7e5c05986dccc4da3476ab2
Jacob Templeton <jwctempleton@gmail.com>
2022-03-06 14:44:49 -0800

    Add solutions to Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+  let firstNum = 0;
+  let secondNum = 1;
+  if (count < 0) {
+    return "OOPS";
+  }
+  for (let i = 1; i < count; i++) {
+    let temp = secondNum;
+    secondNum = firstNum + secondNum;
+    firstNum = temp;
+  }
+  return secondNum;
 };
 
 // Do not edit below this line
