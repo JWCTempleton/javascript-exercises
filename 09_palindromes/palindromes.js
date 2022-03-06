4ebc419bf0afad11d7fa5fff11d234e798c71188
Jacob Templeton <jwctempleton@gmail.com>
2022-03-06 13:47:28 -0800

    Add solutions to Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+// take string, remove special characters
+// make new variable set to string reversed
+// loop through each index of string from beginning and compare to
+// LAST index of reversed string
+// if they are the same the whole way return true
+// else return false
 
+const palindromes = function (str) {
+  let newStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let reversedStr = newStr.toLowerCase().split("").reverse().join("");
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] !== reversedStr[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
